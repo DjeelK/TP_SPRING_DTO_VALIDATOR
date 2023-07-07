79d94c424a852eea60373651fc08d6757b716a9d
TRUFFIN Cassandre <Truffincassandre@gmail.com>
2023-07-07 11:51:02 +0200

    Ajout exceptions

---
+++ src/main/java/tp_spring_dto_validator/tp_spring_dto_validator/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package tp_spring_dto_validator.tp_spring_dto_validator.exception;
+
+public class NotFoundException extends Exception{
+    public NotFoundException() {
+        super("Not Found");
+    }
+}
