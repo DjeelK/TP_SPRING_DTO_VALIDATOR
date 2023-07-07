7d24d21c283744a137dc41863a4b04ac1b59fe48
TRUFFIN Cassandre <Truffincassandre@gmail.com>
2023-07-07 11:49:35 +0200

    Ajout classes controller/service

---
+++ src/main/java/tp_spring_dto_validator/tp_spring_dto_validator/controller/DislickController.java
@@ -0,0 +1,7 @@
+package tp_spring_dto_validator.tp_spring_dto_validator.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DislickController {
+}
---
+++ src/main/java/tp_spring_dto_validator/tp_spring_dto_validator/service/UserService.java
@@ -0,0 +1,7 @@
+package tp_spring_dto_validator.tp_spring_dto_validator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
